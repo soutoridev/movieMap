c54f07e349911bb8543aeff964ef976fc8855496
Ricardo <soutoridec@gmail.com>
2023-09-17 05:42:49 -0300

    Remove variavel das variaveis globais de hambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: true,
   url:'https://api.themoviedb.org/3/movie/157336?api_key=4fee75766f309c5c4445f05089efe67e',
   tmdbUrlImage:'https://image.tmdb.org/t/p/original',
-  production: false,
   tmdbApiUrl: 'https://api.themoviedb.org/3',
   tmdbkey:'4fee75766f309c5c4445f05089efe67e',
   tmdbToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZmVlNzU3NjZmMzA5YzVjNDQ0NWYwNTA4OWVmZTY3ZSIsInN1YiI6IjY1MDNiY2IwZDdkY2QyMDBhY2IyMDE5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pexRYRsh6ZvS7NuFMx9TLYsPCuRljioEp5bJd0ADMsU',
